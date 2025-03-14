db00115c6213e2c0cfbb1702512881e0291a4005
Rustam Shafikov <mitaleko@gmail.com>
2025-03-14 14:45:29 +0500

    Formatting fixed

---
+++ tests/test_interact.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import patch
+from src.user_interaction import interact
+from src.parser import HH
+
+class TestInteract(unittest.TestCase):
+
+    @patch('builtins.input', return_value='5')
+    # Патчим input и имитируем ввод '5'
+    def test_interact_incorrect_selection(self, mock_input):
+        with patch('builtins.print') as mock_print:
+        # Патчим print, чтобы перехватить вывод
+            interact()
+            mock_print.assert_called_with('Неверный выбор. Программа завершает работу.')
+
+
+
+
+
